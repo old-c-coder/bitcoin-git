13f29e5e85bd8a846e8545cff1ff3984d572bed1
Gavin Andresen <gavinandresen@gmail.com>
2012-01-27 18:53:53 -0500

    Bad merge dropped fee calculation for block reward

--- main.cpp
+++ main.cpp
@@ -1589,6 +1589,8 @@ bool CBlock::ConnectBlock(CTxDB& txdb, CBlockIndex* pindex)
                     return error("ConnectBlock() : too many sigops");
             }
 
+            nFees += tx.GetValueIn(mapInputs)-tx.GetValueOut();
+
             if (!tx.ConnectInputs(mapInputs, mapQueuedChanges, posThisTx, pindex, true, false, fStrictPayToScriptHash))
                 return false;
         }
